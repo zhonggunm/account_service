e8a80f650a19a483c22dd5073246a7b13c61877d
zhonggunm@hotmail.com <zhonggunm@hotmail.com>
2020-03-06 15:37:52 -0700

    test git

--- src/main/java/com/zhong/myapp/sql_user_service/SqlUserServiceApplication.java
+++ src/main/java/com/zhong/myapp/sql_user_service/SqlUserServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SqlUserServiceApplication {
+	
+	//test git
 
 	public static void main(String[] args) {
 		System.out.print("SqlUserService\n");
